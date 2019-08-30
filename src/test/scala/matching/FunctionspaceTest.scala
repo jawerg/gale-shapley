16b224dee2210527c18dc34281d8e3cfb41c3087
jawerg <janwergula@gmail.com>
2019-08-30 18:21:05 +0200

    symmetric solver test #1

--- src/test/scala/matching/FunctionspaceTest.scala
+++ src/test/scala/matching/FunctionspaceTest.scala
@@ -18,7 +18,28 @@ class FunctionspaceTest extends FunSuite {
       3 -> List( 4, 1, 2, 3 ),
       4 -> List( 3, 2, 1, 4 )
     )
-    val alloc: Alloc = Map( )
-    assert( GaleShapleySolver( prefM, prefW ).toList.sortBy( _._1 ).toMap == Map( 1 -> 3, 2 -> 4, 3 -> 1, 4 -> 2 ) )
+    assert(
+      GaleShapleySolver( prefM, prefW ).toList.sortBy( _._1 ).toMap
+        === Map( 1 -> 3, 2 -> 4, 3 -> 1, 4 -> 2 )
+    )
+  }
+
+  test( "Symmetry in unique match problem" ) {
+    val prefM: Pref = Map[ Boy, List[ Girl ] ](
+      1 -> List( 1, 2, 3, 4 ),
+      2 -> List( 1, 4, 3, 2 ),
+      3 -> List( 2, 1, 3, 4 ),
+      4 -> List( 4, 2, 3, 1 )
+    )
+    val prefW: Pref = Map[ Girl, List[ Boy ] ](
+      1 -> List( 4, 3, 1, 2 ),
+      2 -> List( 2, 4, 1, 3 ),
+      3 -> List( 4, 1, 2, 3 ),
+      4 -> List( 3, 2, 1, 4 )
+    )
+    assert(
+      GaleShapleySolver( prefM, prefW ).toList.sortBy( _._1 ).toMap
+        === GaleShapleySolver( prefW, prefM ).toList.sortBy( _._1 ).toMap
+    )
   }
 }
