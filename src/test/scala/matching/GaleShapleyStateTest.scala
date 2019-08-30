f02252cb9c20a4751f4a96b2b01f899d9134efa6
jawerg <janwergula@gmail.com>
2019-08-30 17:35:42 +0200

    added test for empty matches #1 #6

--- src/test/scala/matching/GaleShapleyStateTest.scala
+++ src/test/scala/matching/GaleShapleyStateTest.scala
@@ -1,8 +1,16 @@
 import org.scalatest.FunSuite
 import matching.GaleShapleyState
+import matching.TypeDef._
 
 class GaleShapleyStateTest extends FunSuite {
   test( "GaleShapleyState.matches" ) {
     assert( GaleShapleyState( Map( 1 -> List( 1 ) ), Map( 1 -> List( 1 ) ), Map( ) ).matches === Map( 1 -> 1 ) )
+  }
+
+  test( "Empty matches" ) {
+    val prefM = Map( 1 -> List( 2 ), 2 -> List( 1 ) )
+    val prefW = Map( 1 -> List( 1 ), 2 -> List( 2 ) )
+    val alloc: Alloc = Map( )
+    assert( GaleShapleyState( prefM, prefW, alloc ).matches === Map( ) )
   }
 }
