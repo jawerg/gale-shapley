81bdd97722c3628f3a50a4f8ce0e6b26ec1d2420
jawerg <janwergula@gmail.com>
2019-09-01 20:38:50 +0200

    set up new test to ensure global instead of local stability

--- src/test/scala/matching/FunctionspaceTest.scala
+++ src/test/scala/matching/FunctionspaceTest.scala
@@ -1,8 +1,8 @@
 package matching
 
-import org.scalatest.FunSuite
+import matching.Functionspace._
 import matching.TypeDef._
-import matching.Functionspace.GaleShapleySolver
+import org.scalatest.FunSuite
 
 class FunctionspaceTest extends FunSuite {
   test( "Example 2 from Paper" ) {
@@ -40,6 +40,29 @@ class FunctionspaceTest extends FunSuite {
     assert(
       GaleShapleySolver( prefM, prefW ).toList.sortBy( _._1 ).toMap
         === GaleShapleySolver( prefW, prefM ).toList.sortBy( _._1 ).toMap
+    )
+  }
+
+  // Late Reallocation assures that the program does end in "locally stable"
+  // allocations, but traverses down the search tree until the match 4 -> 3 is
+  // finally found.
+  test( "Late Reallocation" ) {
+    val prefM: Pref = Map(
+      1 -> List( 1, 2, 3 ),
+      2 -> List( 1, 2, 3 ),
+      3 -> List( 1, 2, 3 ),
+      4 -> List( 1, 2, 3 )
+    )
+    val prefW: Pref = Map(
+      1 -> List( 1, 4 ),
+      2 -> List( 2, 4 ),
+      3 -> List( 4, 3 )
+    )
+    val alloc: Alloc = Map( 1 -> 1, 2 -> 2, 3 -> 3 )
+
+    assert(
+      GaleShapleyIter( GaleShapleyState( prefM, prefW, alloc ) )
+        === Map( 1 -> 1, 2 -> 2, 3 -> 4 )
     )
   }
 }
